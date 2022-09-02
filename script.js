41b3394be47250dbcdd9f2f04ed295a85b7237d9
chalghoumimalika <112657750+chalghoumimalika@users.noreply.github.com>
2022-09-02 10:13:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -147,7 +147,7 @@ async function init() {
 //      vocab=json
 //     console.log(json); // this will show the info it in firebug console
 //      });
-  fetch("/MODEL/tokenizer.json")
+  fetch("./MODEL/tokenizer.json")
    .then(response => response.json())
    .then(json => console.log(json));
       
